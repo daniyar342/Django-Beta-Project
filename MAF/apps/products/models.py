921ca167a2d4d6f748dcc996950d525337061564
l9l9n_HP <l9l9ndos@gmail.com>
2023-11-28 19:25:59 +0600

    from ot Uluka

--- MAF/apps/products/models.py
+++ MAF/apps/products/models.py
@@ -4,30 +4,45 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=150)
+    name = models.CharField(verbose_name='Категории', max_length=150)
     slug = models.SlugField()
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
 
     def __str__(self):
         return self.name
+
 
 class SubCategory(models.Model):
-    name = models.CharField(max_length=130)
+    name = models.CharField(verbose_name='Подкатегории', max_length=150)
     slug = models.SlugField()
-    category = models.ForeignKey(Category,on_delete=models.CASCADE,null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+
+    class Meta:
+        verbose_name = 'Подкатегория'
+        verbose_name_plural = 'Подкатегории'
+
     def __str__(self):
         return self.name
 
+
 class Product(models.Model):
-    name = models.CharField(max_length=255)
-    compound = models.TextField()
-    description = models.TextField()
-    applying = models.TextField()
-    waiting_time = models.TextField()
-    release = models.TextField()
-    storage_date = models.TextField()
-    storage_conditions = models.TextField()
+    name = models.CharField(verbose_name='Название продукта', max_length=255)
+    compound = models.TextField(verbose_name='Состав')
+    description = models.TextField(verbose_name='Описание')
+    applying = models.TextField(verbose_name='Применение')
+    waiting_time = models.TextField(verbose_name='Период ожидания')
+    release_form = models.TextField(verbose_name='Форма выпуска')
+    storage_date = models.TextField(verbose_name='Состав')
+    storage_conditions = models.TextField(verbose_name='Срок хранения')
     # category = models.ForeignKey(Category,null=True,blank=True)
-    sub_category = models.ForeignKey(SubCategory,on_delete=models.SET_NULL,null=True)
+    sub_category = models.ForeignKey(SubCategory, on_delete=models.SET_NULL, null=True, verbose_name='Какой подкатегории относится?')
+
+    class Meta:
+        verbose_name = 'Препарат'
+        verbose_name_plural = 'Препараты'
 
     def __str__(self):
         return self.name
