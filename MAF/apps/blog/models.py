ae522d1eb332d40077cf92ae9424cfd1c41576ba
daniyar342 <tolomushev33@gmail.com>
2023-12-01 19:49:22 +0600

    BLOG SERIALIZERS

--- MAF/apps/blog/models.py
+++ MAF/apps/blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Events(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.CharField(max_length=150)
+
+class Public(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.CharField(max_length=200)
+
+class New_Products(models.Model):
+    pass
+
+class Enxibitation_Calendar(models.Model):
+    period = models.CharField(max_length=100)
+    data_of_participation = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    name_exhibition = models.TextField()
