d30b6e87542415f8a611211d96428cfbab3ce6e5
daniyar342 <tolomushev33@gmail.com>
2023-12-01 19:49:22 +0600

    BLOG SERIALIZERS

---
+++ MAF/apps/blog/serializer.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from .models import *
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Events
+        fields = '__all__'
--- MAF/apps/blog/views.py
+++ MAF/apps/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import generics
+from .models import Events
+from .serializer import EventSerializer
+class BlogEvents(generics.ListAPIView):
+    queryset = Events.objects.all()
+    serializer_class = EventSerializer
