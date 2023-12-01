1f55872b291cb827f971f5be9c9df98b88289af4
l9l9n_HP <l9l9ndos@gmail.com>
2023-12-01 19:49:33 +0600

    another fix

--- MAF/apps/products/urls.py
+++ MAF/apps/products/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path("list_products/", Index.as_view(), name="list"),
-    path("api/v1/productlist/",Index.as_view()),
-    ]
+    # path("list_products/", Index.as_view(), name="list"),
+    path("api/v1/productlist/", Index.as_view()),
+]
