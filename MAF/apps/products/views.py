59c42347f14970dd4b3d8a1b9ae8e95226cb6b24
l9l9n_HP <l9l9ndos@gmail.com>
2023-12-01 19:49:33 +0600

    another fix

--- MAF/apps/products/views.py
+++ MAF/apps/products/views.py
@@ -4,6 +4,7 @@ from rest_framework import generics
 from .models import Product
 from .serializer import ProductSeializer
 
+
 class Index(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSeializer
