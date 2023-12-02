6e033774356a3affa5cce63a0c6676511ec26c3e
l9l9n_HP <l9l9ndos@gmail.com>
2023-12-02 19:49:17 +0600

    Prelas Commit

--- MAF/apps/blog/views.py
+++ MAF/apps/blog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .models import Events
-from .serializer import EventSerializer
+from .models import Events, Public
+from .serializer import EventSerializer, PublicSerializer
+
+
 class BlogEvents(generics.ListAPIView):
     queryset = Events.objects.all()
     serializer_class = EventSerializer
+
+
+class PublicBlogView(generics.ListAPIView):
+    queryset = Public.objects.all()
+    serializer_class = PublicSerializer
