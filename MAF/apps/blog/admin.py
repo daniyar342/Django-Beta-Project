e5777ce25b26a5138ee3a98a38197c7aa92a90ef
l9l9n_HP <l9l9ndos@gmail.com>
2023-12-02 19:49:17 +0600

    Prelas Commit

--- MAF/apps/blog/admin.py
+++ MAF/apps/blog/admin.py
@@ -1,17 +1,39 @@
 from django.contrib import admin
 from .models import *
+
+
 @admin.register(Events)
 class BlogEventsAdmin(admin.ModelAdmin):
-    pass
+    list_display = [
+        'title',
+        'description',
+        'created_at',
+        'image'
+    ]
+
 
 @admin.register(Public)
 class BlogPublicAdmin(admin.ModelAdmin):
-    pass
+    list_display = [
+        'title',
+        'description',
+        'created_at',
+        'image'
+    ]
+
 
 @admin.register(New_Products)
 class BlogNewProductsAdmin(admin.ModelAdmin):
-    pass
+    list_display = [
+        'product_name'
+    ]
+
 
 @admin.register(Enxibitation_Calendar)
 class BlogCalendarAdmin(admin.ModelAdmin):
-    pass
+    list_display = [
+        'period',
+        'data_of_participation',
+        'location',
+        'name_exhibition'
+    ]
