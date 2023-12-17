03551021e6e5a5f22d74f87eaa61a77efc6f00fe
daniyar342 <tolomushev33@gmail.com>
2023-12-17 14:48:28 +0600

    has made api for blog apps and with routers that in config/api

--- MAF/apps/blog/views.py
+++ MAF/apps/blog/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, mixins, viewsets
+from rest_framework.generics import GenericAPIView
+from rest_framework.viewsets import GenericViewSet
+
 from .models import Events, Public
 from .serializer import EventSerializer, PublicSerializer
 
 
-class BlogEvents(generics.ListAPIView):
+class BlogEventsView( mixins.RetrieveModelMixin,
+                   mixins.UpdateModelMixin,
+                   mixins.DestroyModelMixin,
+                   mixins.ListModelMixin,
+                   GenericViewSet):
     queryset = Events.objects.all()
     serializer_class = EventSerializer
 
 
-class PublicBlogView(generics.ListAPIView):
+class PublicBlogView( mixins.RetrieveModelMixin,
+                   mixins.UpdateModelMixin,
+                   mixins.DestroyModelMixin,
+                   mixins.ListModelMixin,
+                   GenericViewSet):
     queryset = Public.objects.all()
     serializer_class = PublicSerializer
 
- # kuhiu
